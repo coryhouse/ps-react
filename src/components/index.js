e8c5a8623b4dfdb2fd4fac2140b05bf68e2b5001
Cory House <housecor@gmail.com>
2017-04-30 01:15:30 -0500

    ready for publish

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as EyeIcon} from './EyeIcon';
+export {default as ProgressBar} from './ProgressBar';
+export {default as Label} from './Label';
+export {default as TextInput} from './TextInput';
+export {default as PasswordInput} from './PasswordInput';
+export {default as RegistrationForm} from './RegistrationForm';
