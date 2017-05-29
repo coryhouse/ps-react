2b8e19ee4e3b8f2b39eabdfb9afbec4e56105468
Cory House <housecor@gmail.com>
2017-05-29 13:21:30 -0500

    Disable ModuleScopePlugin to support import from /config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -84,12 +84,10 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-      
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       'ps-react': path.resolve(__dirname, '../src/components')
-
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
@@ -97,7 +95,7 @@ module.exports = {
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
-      new ModuleScopePlugin(paths.appSrc),
+      // new ModuleScopePlugin(paths.appSrc),
     ],
   },
   module: {
