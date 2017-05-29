1ec6827b992f249a819c618c06ca3bae855a3f67
Cory House <housecor@gmail.com>
2017-05-29 09:59:59 -0500

    Revert componentData path change

--- src/docs/Docs.js
+++ src/docs/Docs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from './Navigation';
 import ComponentPage from './ComponentPage';
-import componentData from './componentData';
+import componentData from '../../config/componentData';
 
 export default class Docs extends React.Component {
   constructor(props) {
