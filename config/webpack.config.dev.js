6b4ff021c015cff9a9b25c9353a0891aa869727e
Cory House <housecor@gmail.com>
2017-05-29 13:24:04 -0500

    Add ps-react alias

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -89,6 +89,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'ps-react': path.resolve(__dirname, '../src/components')
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
