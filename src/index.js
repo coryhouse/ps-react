a49cfb378f55e589d68a87bc622c09d7d12b8eb9
Cory House <housecor@gmail.com>
2017-07-02 08:53:49 -0500

    Import prism and reference desired prism theme css in node_modules

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import '../node_modules/highlight.js/styles/ocean.css';
+import './prism';
+import 'prismjs/themes/prism-okaidia.css';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
 registerServiceWorker();
